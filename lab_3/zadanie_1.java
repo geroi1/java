f749d18e09199cfdeb994a9bf1cb1a3c1cf24fd7
geroi1 <99471303+geroi1@users.noreply.github.com>
2022-11-16 11:15:35 +0700

    Update zadanie_1.java

--- lab_3/zadanie_1.java
+++ lab_3/zadanie_1.java
@@ -1,17 +1,17 @@
 package lab_3;
 import java.util.Scanner;
 
-public class zadanie_1 {
+public class zadanie1 {
 
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите n: ");
-        int n = in.nextInt();
-        int r = 0;
-        for (int i = 0; i < n; i++) {
-            r = (int) Math.pow (2, n);
-        }
+        double n = in.nextDouble();
+        double r = 0;
+//        for (int i = 0; i < n; i++) {
+            r = (double) Math.pow (2, n);
+//        }
         System.out.println(r);
     }
     
